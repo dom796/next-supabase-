00b173fb65c4addabc1f94af3a6c5925e5e0d3e4
dom796 <dom796@users.noreply.github.com>
2026-03-23 19:19:32 +0100

    initial commit

---
+++ src/components/logo.tsx
@@ -0,0 +1,18 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+export function Logo() {
+  return (
+    <Link href='/' className='flex w-fit items-center gap-2'>
+      <Image
+        src='/logo.png'
+        width={40}
+        height={40}
+        priority
+        quality={100}
+        alt='BoilerBase logo mark'
+      />
+      <span className='font-alt text-xl text-white'>BoilerBase</span>
+    </Link>
+  );
+}
